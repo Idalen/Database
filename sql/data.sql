b137636f46d4dca689343414571514eaed8d32b6
madukm <kawakamimadu@gmail.com>
2021-07-12 20:46:38 -0300

    adding data in the db

--- sql/data.sql
+++ sql/data.sql
@@ -9,10 +9,9 @@ INSERT INTO pais
 
 INSERT INTO parque_tematico  
     VALUES(
-        -12345678911, 
+        12345678911, 
         'Argentina',
         'Parque Didi',
-        'Buenos Aires',
         150.0,
         700,
         '08:00:00','22:00:00',
@@ -27,7 +26,6 @@ INSERT INTO parque_tematico
         12345678910, 
         'Angola',
         'Parque Madu',
-        'Luanda',
         99.99,
         NULL,
         '08:00:00','22:00:00',
@@ -39,7 +37,255 @@ INSERT INTO parque_tematico
         '08:00:00','22:00:00'
         );
 
-    
+
+-- Insercao de dados em restaurante
+INSERT INTO restaurante
+    VALUES
+    (
+        12345678912, -- documento restaurante
+        12345678911, -- documento parque
+        'RESTAURANTE DO DIOGO', -- nome restaurante
+        numrange(100, 500, '[)'),-- faixa de preço,
+        '08:00:00','21:00:00', -- ini fim dom
+        '09:00:00','12:00:00', -- ini fim seg
+        '10:00:00','15:00:00', -- ini fim ter
+        '09:00:00','12:00:00', -- ini fim qua
+        '08:00:00','19:00:00', -- ini fim qui
+        '09:00:00','12:00:00', -- ini fim sex
+        '08:00:00','22:00:00'  -- ini fim sab
+    ),
+    (
+        12345678913,
+        12345678910,
+        'RESTAURANTE DA MADU',
+        numrange(100, 500, '[)'),
+        '08:00:00','21:00:00',
+        '09:00:00','12:00:00',
+        '10:00:00','15:00:00',
+        '09:00:00','12:00:00',
+        '08:00:00','19:00:00',
+        '09:00:00','12:00:00',
+        '08:00:00','22:00:00'
+    );
+
+-- Insercao dados tabela cozinha
+INSERT INTO cozinha 
+    VALUES
+    (
+        12345678912, -- documento restaurante
+        'FRANCESA' -- cozinha
+    ),
+    (
+        12345678912,
+        'ITALIANA'
+    ),
+    (
+        12345678913,
+        'JAPONESA'
+    ),
+    (
+        12345678913,
+        'CHINESA'
+    ),
+    (
+        12345678913,
+        'COREANA'
+    );
+
+-- insercao de dados em hotel
+INSERT INTO hotel
+    VALUES
+    (
+        12345678914, -- documento hotel
+        12345678911, -- fk parque
+        'HOTEL DO DIOGO', -- nome hotel
+        0, -- total_quartos
+        0 -- total_vagas
+    ),
+    (
+        12345678915,
+        12345678910,
+        'HOTEL DA MADU',
+        0,
+        0
+    );
+
+-- insercaod e dados em servico hotel
+INSERT INTO servico_hotel
+    VALUES
+    (
+        12345678914, -- documento hotel
+        'LAVANDERIA' -- serviço
+    ),
+    (
+        12345678914,
+        'ACADEMIA'
+    ),
+    (
+        12345678914,
+        'DESJEJUM'
+    ),
+    (
+        12345678915,
+        'DESJEJUM'
+    ),
+    (
+        12345678915,
+        'PISCINA'
+    ),
+    (
+        12345678915,
+        'SAUNA'
+    );
+
+-- insercao de dados na tabela quarto
+INSERT INTO quarto
+    VALUES 
+    (
+        12345678914, -- documento do hotel
+        1, -- número do quarto
+        2
+    ),
+    (
+        12345678914,
+        2,
+        1
+    ),
+    (
+        12345678914, 
+        3,
+        3
+    ),
+    (
+        12345678914, 
+        4,
+        2
+    ),
+    (
+        12345678915,
+        1,
+        2
+    ),
+    (
+        12345678915,
+        2,
+        1
+    ),
+    (
+        12345678915,
+        3,
+        4
+    );
+
+-- insercao de dados em turista
+INSERT INTO turista
+    VALUES
+    (
+        12345678916, -- passaporte
+        'DIOGO', -- nome 
+        '2000-03-27', -- data de nascimento
+        '(16)999999999' -- telefone
+    ),
+    (
+        12345678917,
+        'MADU',
+        '2001-04-19',
+        '(16)999999998'
+    ),
+    (
+        12345678918,
+        'DANIEL',
+        '2001-09-18',
+        '(16)999999997'
+    );
+
+-- insercao de dados restricoes alimentares
+INSERT INTO restricoes_alimentares
+    VALUES
+    (
+        12345678917,
+        'ALERGIA CAMARAO'
+    ),
+    (
+        12345678917,
+        'CELIACA'
+    ),
+    (
+        12345678918,
+        'ALERGIA AMENDOIM'
+    ),
+    (
+        12345678918,
+        'VEGANO'
+    );
+
+INSERT INTO necessidades_especiais
+    VALUES
+    (
+        12345678916, -- passaporte turista
+        'CADEIRANTE' -- necessidade
+    ),
+    (
+        12345678917,
+        'DEFICIENTE VISUAL'
+    );
+
+INSERT INTO avaliacao
+    VALUES
+    (
+        12345678916, -- passaporte turista
+        12345678912,
+        5
+    ),
+    (
+        12345678916,
+        12345678913,
+        4.5
+    ),
+    (
+        12345678917,
+        12345678913,
+        4
+    ),
+    (
+        12345678918,
+        12345678913,
+        4.8
+    ),
+    (
+        12345678918,
+        12345678912,
+        1
+    );
+
+-- insercao de dados hospedagem
+INSERT INTO hospedagem
+    VALUES 
+    (
+        12345678918, -- turista
+        12345678914, -- hotel
+        2, -- quarto
+        '[2021-05-06 13:00, 2021-05-07 10:00)' -- duracao
+
+    ),
+    (
+        12345678917, -- turista
+        12345678915,
+        2,
+        '[2021-05-20 13:00, 2021-05-23 10:30)'
+    ),
+    (
+        12345678916, -- turista
+        12345678915,
+        1,
+        '[2021-05-10 13:00, 2021-05-15 09:30)'
+    );
+
+--  INSERT INTO grupo_turistas
+--    VALUES
+--    (),
+--    (),
+--    ();
 
 
 
--- sql/schemas.sql
+++ sql/schemas.sql
@@ -17,7 +17,7 @@ CREATE TABLE parque_tematico(
 	pais VARCHAR(30) NOT NULL,
 	nome VARCHAR(30) NOT NULL,
 	preco NUMERIC(5, 2) CHECK (preco > 0),
-	lotacao_maxima INTEGER CHECK(lotacao_maximo > 0),
+	lotacao_maxima INTEGER CHECK(lotacao_maxima > 0),
 	dom_inicio 	TIME,
 	dom_fim 	TIME,
 	seg_inicio 	TIME,
@@ -47,9 +47,8 @@ CREATE TABLE parque_tematico(
 CREATE TABLE restaurante(
 	documento NUMERIC(11, 0) PRIMARY KEY CHECK(documento > 0),
 	parque NUMERIC(11, 0) NOT NULL,
-	CONSTRAINT fk_parque FOREIGN KEY(parque) REFERENCES parque_tematico(documento), 
 	nome VARCHAR(255),
-	faixa_preco VARCHAR(255),
+	faixa_preco NUMRANGE,
 	dom_inicio 	TIME,
 	dom_fim 	TIME,
 	seg_inicio 	TIME,
@@ -64,6 +63,7 @@ CREATE TABLE restaurante(
 	sex_fim 	TIME,
 	sab_inicio 	TIME,
 	sab_fim 	TIME,
+	CONSTRAINT fk_parque FOREIGN KEY(parque) REFERENCES parque_tematico(documento), 
 	-- check de horario
 	CONSTRAINT valid_dom CHECK (dom_inicio < dom_fim),
 	CONSTRAINT valid_seg CHECK (seg_inicio < seg_fim),
@@ -77,8 +77,8 @@ CREATE TABLE restaurante(
 -- Criando tabela dos tipos de cozinha de um restaurante 
 CREATE TABLE cozinha(
 	restaurante NUMERIC(11, 0),
-	CONSTRAINT fk_rest FOREIGN KEY(restaurante) REFERENCES restaurante(documento), 
 	tipo_cozinha VARCHAR(30),
+	CONSTRAINT fk_rest FOREIGN KEY(restaurante) REFERENCES restaurante(documento), 
 	CONSTRAINT pk_cozinha PRIMARY KEY(restaurante, tipo_cozinha)
 );
 
@@ -86,27 +86,26 @@ CREATE TABLE cozinha(
 CREATE TABLE hotel(
 	documento NUMERIC(11, 0) PRIMARY KEY,
 	parque NUMERIC(11, 0) NOT NULL,
-	CONSTRAINT fk_parque FOREIGN KEY(parque) REFERENCES parque_tematico(documento),
 	nome VARCHAR(30),
-	total_quartos INTEGER CHECK (total_quartos > 0), -- total de quartos nao pode ser negativo
+	total_quartos INTEGER CHECK (total_quartos >= 0), -- total de quartos nao pode ser negativo
 	total_vagas INTEGER CHECK (total_quartos <= total_vagas), --sempre ha, no minimo, uma vaga, ocupada ou não, para cada quarto
-	quartos_livres INTEGER, -- ATRIBUTO DERIVADO
-	vagas_livres INTEGER -- ATRIBUTO DERIVADO
+	CONSTRAINT fk_parque FOREIGN KEY(parque) REFERENCES parque_tematico(documento)
 );
 
 -- Criando tabela dos serviços do hotel
 CREATE TABLE servico_hotel(
 	hotel NUMERIC(11, 0),
-	CONSTRAINT fk_hotel FOREIGN KEY(hotel) REFERENCES hotel(documento),
 	servico VARCHAR(30),
+	CONSTRAINT fk_hotel FOREIGN KEY(hotel) REFERENCES hotel(documento),
 	CONSTRAINT pk_servico_hotel PRIMARY KEY(hotel, servico)
 );
 
 -- Criando tabela do Quarto
 CREATE TABLE quarto(
 	hotel NUMERIC(11, 0),
-	CONSTRAINT fk_hotel FOREIGN KEY(hotel) REFERENCES hotel(documento),
 	numero INTEGER CHECK(numero > 0), --O numero do quarto não pode ser 0 ou negativo 
+	vagas INTEGER CHECK(vagas > 0),
+	CONSTRAINT fk_hotel FOREIGN KEY(hotel) REFERENCES hotel(documento),
 	CONSTRAINT pk_quarto PRIMARY KEY(hotel, numero)
 );
 
@@ -115,14 +114,14 @@ CREATE TABLE turista(
 	passaporte NUMERIC(11, 0) PRIMARY KEY CHECK(passaporte > 0),
 	nome VARCHAR(30) NOT NULL,
 	data_nascimento DATE NOT NULL, -- tipo DATE
-	telefone VARCHAR(20),
+	telefone VARCHAR(20)
 );
 
 -- Criando tabela de Restrições Alimentares do Turista
 CREATE TABLE restricoes_alimentares(
 	turista NUMERIC(11, 0),
-	CONSTRAINT fk_turista FOREIGN KEY(turista) REFERENCES turista(passaporte),
 	restricao VARCHAR(30),
+	CONSTRAINT fk_turista FOREIGN KEY(turista) REFERENCES turista(passaporte),
 	CONSTRAINT pk_restricoes_alimentares PRIMARY KEY(turista, restricao)
 );
 
@@ -130,8 +129,8 @@ CREATE TABLE restricoes_alimentares(
 -- Criando tabela de Necessidades Especiais do Turista
 CREATE TABLE necessidades_especiais(
 	turista NUMERIC(11, 0),
-	CONSTRAINT fk_turista FOREIGN KEY(turista) REFERENCES turista(passaporte),
 	necessidade VARCHAR(30),
+	CONSTRAINT fk_turista FOREIGN KEY(turista) REFERENCES turista(passaporte),
 	CONSTRAINT pk_necessidades_especiais PRIMARY KEY(turista, necessidade)
 );
 
@@ -139,41 +138,39 @@ CREATE TABLE necessidades_especiais(
 -- Criando tabela da Avaliação
 CREATE TABLE avaliacao(
 	turista NUMERIC(11, 0),
-	CONSTRAINT fk_turista FOREIGN KEY(turista) REFERENCES turista(passaporte),
 	restaurante NUMERIC(11, 0),
+	nota NUMERIC(2, 1) NOT NULL CHECK (nota>=0 and nota <= 5),
+	CONSTRAINT fk_turista FOREIGN KEY(turista) REFERENCES turista(passaporte),
 	CONSTRAINT fk_restaurante FOREIGN KEY(restaurante) REFERENCES restaurante(documento),
-	nota NUMERIC(1, 1) NOT NULL CHECK (nota>=0 and nota <= 5),
 	CONSTRAINT pk_avaliacao PRIMARY KEY(turista, restaurante)
 );
 
 -- Criando tabela da Hospedagem 
 CREATE TABLE hospedagem(
 	turista NUMERIC(11, 0),
-	CONSTRAINT fk_turista FOREIGN KEY(turista) REFERENCES turista(passaporte),
-	hotal NUMERIC(11, 0),
+	hotel NUMERIC(11, 0),
 	quarto INTEGER,
-	checkin TIMESTAMP,
-	checkout TIMESTAMP,
-	CONSTRAINT fk_quarto FOREIGN KEY(quarto, hotel) REFERENCES quarto(numero, hotel)
-	CONSTRAINT valid_hospedagem_timestamp CHECK (checkin < checkout)
-	CONSTRAINT pk_hospedagem PRIMARY KEY(turista, quarto, hotel, checkin, checkout)
+	duracao TSRANGE,
+	CONSTRAINT fk_turista FOREIGN KEY(turista) REFERENCES turista(passaporte),
+	CONSTRAINT fk_quarto FOREIGN KEY(quarto, hotel) REFERENCES quarto(numero, hotel),
+	--CONSTRAINT valid_hospedagem_timestamp CHECK (checkin < checkout),
+	CONSTRAINT pk_hospedagem PRIMARY KEY(turista, quarto, hotel, duracao)
 );
 
 -- Criando tabela do Grupo de Turistas 
 CREATE TABLE grupo_turistas(
 	admin NUMERIC(11, 0),
-	CONSTRAINT fk_admin FOREIGN KEY(admin) REFERENCES turista(passaporte),
 	nome_grupo VARCHAR(30),
-	quantidade INTEGER CHECK (quantidade > 0) --- !Atributo derivado
+	CONSTRAINT fk_admin FOREIGN KEY(admin) REFERENCES turista(passaporte),
 	CONSTRAINT pk_grupo_turistas PRIMARY KEY(admin, nome_grupo)
 );
 
 -- Criando tabela de Participação
 CREATE TABLE participacao(
 	turista NUMERIC(11, 0),
-	CONSTRAINT fk_turista FOREIGN KEY(turista) REFERENCES turista(passaporte),
 	admin_grupo NUMERIC(11, 0),
 	nome_grupo VARCHAR(30),
+	CONSTRAINT fk_turista FOREIGN KEY(turista) REFERENCES turista(passaporte),
 	CONSTRAINT fk_grupo FOREIGN KEY(admin_grupo, nome_grupo) REFERENCES grupo_turistas(admin, nome_grupo),
 	CONSTRAINT pk_participacao PRIMARY KEY(turista, admin_grupo, nome_grupo)
 );
@@ -183,14 +180,13 @@ CREATE TABLE participacao(
 CREATE TABLE viagem(
 	admin_grupo NUMERIC(11, 0),
 	nome_grupo VARCHAR(255),
-	CONSTRAINT fk_grupo FOREIGN KEY(admin_grupo, nome_grupo) REFERENCES grupo_turistas(admin, nome_grupo),
 	data_partida TIMESTAMP,
 	data_chegada TIMESTAMP,
 	pais_origem VARCHAR(30) NOT NULL,
-	CONSTRAINT fk_pais_origem FOREIGN KEY(pais_origem) REFERENCES pais(nome_pais),
 	pais_destino VARCHAR(30) NOT NULL,
+	CONSTRAINT fk_grupo FOREIGN KEY(admin_grupo, nome_grupo) REFERENCES grupo_turistas(admin, nome_grupo),
+	CONSTRAINT fk_pais_origem FOREIGN KEY(pais_origem) REFERENCES pais(nome_pais),
 	CONSTRAINT fk_pais_destino FOREIGN KEY(pais_destino) REFERENCES pais(nome_pais), 
-	duracao INTEGER, --Atributo derivado
 	CONSTRAINT pk_viagem PRIMARY KEY(admin_grupo, nome_grupo, data_partida, data_chegada)
 );
 
@@ -200,41 +196,41 @@ CREATE TABLE passeio(
 	data DATE UNIQUE, 
 	admin_grupo NUMERIC(11, 0), 
 	nome_grupo VARCHAR(30),
-	CONSTRAINT un_grupo UNIQUE (admin_grupo, nome_grupo),
-	CONSTRAINT fk_grupo FOREIGN KEY(admin_grupo, nome_grupo) REFERENCES grupo_turistas(admin, nome_grupo),
 	parque NUMERIC(11, 0) UNIQUE,
-	CONSTRAINT fk_parque FOREIGN KEY(parque) REFERENCES parque_tematico(documento),
 	nome_guia VARCHAR(30),
-	preco_guia NUMERIC(5, 2) CHECK (preco_guia >=0 ) -- preco-guia tem que ser no minimo 0
+	preco_guia NUMERIC(5, 2) CHECK (preco_guia >=0 ), -- preco-guia tem que ser no minimo 0CONSTRAINT un_grupo UNIQUE (admin_grupo, nome_grupo),
+	CONSTRAINT fk_grupo FOREIGN KEY(admin_grupo, nome_grupo) REFERENCES grupo_turistas(admin, nome_grupo),
+	CONSTRAINT fk_parque FOREIGN KEY(parque) REFERENCES parque_tematico(documento)
+	
 );
 
 -- Criando tabela dos idiomas do(a) guia
 CREATE TABLE idiomas_guia(
 	passeio INTEGER,
-	CONSTRAINT fk_passeio FOREIGN KEY(passeio) REFERENCES passeio(id),
 	idioma VARCHAR(50),
+	CONSTRAINT fk_passeio FOREIGN KEY(passeio) REFERENCES passeio(id),
 	CONSTRAINT pk_idiomas_guia PRIMARY KEY(passeio, idioma)
 );
 
 -- Criando tabela da Atração
 CREATE TABLE atracao(
 	parque NUMERIC(11, 0),
-	CONSTRAINT fk_parque FOREIGN KEY(parque) REFERENCES parque_tematico(documento),
 	nome VARCHAR(30),
 	tipo VARCHAR(30),
 	-- tem que ter disponibilidade aqui, mas n sei oq significa
 	capacidade INTEGER CHECK (capacidade > 0), -- capacidade precisa ser positiva
+	CONSTRAINT fk_parque FOREIGN KEY(parque) REFERENCES parque_tematico(documento),
 	CONSTRAINT pk_atracao PRIMARY KEY(parque, nome)
 );
 
 -- Criando tabela do Evento
 CREATE TABLE evento(
 	passeio INTEGER,
-	CONSTRAINT fk_passeio FOREIGN KEY(passeio) REFERENCES passeio(id),
 	parque_atracao NUMERIC(11, 0),
 	nome_atracao VARCHAR(30),
-	CONSTRAINT fk_atracao FOREIGN KEY(parque_atracao, nome_atracao) REFERENCES atracao(parque, nome), 
 	ingresso CHAR (25) NOT NULL, -- ingresso vai ser uma string com seu id
+	CONSTRAINT fk_passeio FOREIGN KEY(passeio) REFERENCES passeio(id),
+	CONSTRAINT fk_atracao FOREIGN KEY(parque_atracao, nome_atracao) REFERENCES atracao(parque, nome), 
 	CONSTRAINT pk_evento PRIMARY KEY(passeio, parque_atracao, nome_atracao)
 );
 		
@@ -242,9 +238,9 @@ CREATE TABLE evento(
 CREATE TABLE restricoes_atracao(
 	parque_atracao NUMERIC(11, 0),
 	nome_atracao VARCHAR(30),
+	restricao VARCHAR(30), -- ajeitar no MR
 	CONSTRAINT fk_atracao FOREIGN KEY(parque_atracao, nome_atracao) REFERENCES atracao(parque, nome),
-    restricao VARCHAR(30), -- ajeitar no MR
-	CONSTRAINT pk_restricoes_atracao PRIMARY KEY(parque_atracao, nome_atracao, restricao)
+    CONSTRAINT pk_restricoes_atracao PRIMARY KEY(parque_atracao, nome_atracao, restricao)
 );
 
 
