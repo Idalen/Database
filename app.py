5f916bc0f1d7f7181ddadd638cdca22f1949abbc
idalen <danielmartins@usp.br>
2021-07-16 01:21:41 -0300

    app insertion

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import database as db
+import datetime
 
-print("""Bem vindo à aplicação!""")
+print("""Bem vindo à aplicação!\n""")
 
-conn = db.connect()
+# passaporte = input("Informe seu passaporte:")
+# nome = input("Informe seu nome:")
+# data_nascimento = input("Informe sua data de nascimento (yyyy-mm-dd):")
+# telefone = input("Informe seu telefone:")
 
-print(conn)
+# passaporte = 12345678998
+# nome = 'MARCOS'
+# data_nascimento = datetime.date(2000,6,17)
+# telefone = '(73)999980423'
+
+# if not db.inserir(passaporte, nome, data_nascimento, telefone):
+#     print(f"{nome} ({passaporte}) foi adicionado ao banco.")
+
+db.consultar()
--- database.py
+++ database.py
@@ -4,21 +4,74 @@ import psycopg2
 from config import config
 from read_sql import read_sql
 
-def connect():
-    """ Connect to the PostgreSQL database server """
+def inserir(passaporte, nome, data_nascimento, telefone=None):
+
+    sql = """INSERT INTO turista(passaporte,nome,data_nascimento,telefone)
+                VALUES(%s,%s,%s,%s);"""
     conn = None
-
+    erro = 0
     try:
-        # read connection parameters
+        # read database configuration
         params = config()
-
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
+        # connect to the PostgreSQL database
         conn = psycopg2.connect(**params)
-		
-        # create a cursor
-    except Exception as err:
-        print ("PostgreSQL Connect() ERROR:", err)
-        conn = None
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the UPDATE  statement
+        cur.execute(sql, (passaporte, nome, data_nascimento, telefone))
+        # get the number of updated rows
+        conn.commit()
+        # Close communication with the PostgreSQL database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("ERRO", error)
+        erro = 1
+    finally:
+        if conn is not None:
+            conn.close()
+    return erro
 
-    return conn
+def consultar(pais='ARGENTINA'):
+    sql= """SELECT
+                restaurante.documento,
+                restaurante.nome,
+                restaurante.tipo_cozinha,
+                AVG(a.nota)::NUMERIC(2,1)
+            FROM(
+                SELECT 
+                rest_by_pais.parque, rest_by_pais.nome, c.tipo_cozinha
+                FROM(
+                    SELECT r.documento AS documento, p.nome AS parque, r.nome AS nome
+                    FROM parque_tematico p
+                    INNER JOIN restaurante r
+                        ON p.documento = r.parque
+                    WHERE p.documento = %s
+                )rest_by_pais INNER JOIN cozinha c
+                    ON rest_by_pais.documento = c.restaurante
+                WHERE c.tipo_cozinha = %s
+            )restaurante LEFT OUTER JOIN avaliacao a
+                ON a.restaurante = restaurante.documento
+            GROUP BY 
+                restaurante.documento,
+                restaurante.nome, 
+                restaurante.tipo_cozinha
+            ORDER BY AVG(a.nota);"""
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(sql, (12345678911,'ITALIANA'))
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
